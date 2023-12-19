3efbc52f7510bbb68bbe1d548b72a4434285ac4c
ryanhamburger <147830320+ryanhamburger@users.noreply.github.com>
2023-12-19 13:55:23 -0500

    added button
    added button to all, it go back to the top yessir  YIPEE

--- recipe.js
+++ recipe.js
@@ -1,5 +1,8 @@
 // Get the button:
-let mybutton = document.getElementById("myBtn");
+let mybutton = "";
+window.onload = function() {
+    mybutton = document.getElementById("myBtn");
+}
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
