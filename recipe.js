e9852579a92506526864810ff1c1fceee577eddc
ryanhamburger <147830320+ryanhamburger@users.noreply.github.com>
2023-12-20 13:44:03 -0500

    fixed everything
    all pages fixed, scroll button works, go to meal button works, time works, image works, everything

--- recipe.js
+++ recipe.js
@@ -1,32 +1,37 @@
+
 // Get the button:
 let mybutton = "";
 window.onload = function() {
-    mybutton = document.getElementById("myBtn");
+  Time();
+  mybutton = document.getElementById("myBtn");
+    
+  // When the user scrolls down 60px from the top of the document, show the button
+  window.onscroll = function() {scrollFunction()};
 }
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
 function scrollFunction() {
-  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
   }
 }
 
+
+
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
 function Time()
 {
   var date = new Date();
   var dd = date.getDate(); 
   var mm = date.getMonth() + 1; 
   var yyyy = date.getFullYear(); 
-  var newDate = dd + "-" + mm + "-" +yyyy; 
+  var newDate = dd + "-" + mm + "-" + yyyy; 
   var p = document.getElementById("date"); 
   p.innerHTML = newDate; 
   
